14fa745d8b010df0551ae850b28891a7f939fb15
Arman Oganisian <stablemarkets@users.noreply.github.com>
2017-12-02 13:38:53 -0500

    update documentation for output_all

--- R/keilsim.r
+++ R/keilsim.r
@@ -18,7 +18,7 @@
 #'     performing frequentist g-computation. See Details.
 #' @param output_all logical (TRUE/FALSE). If TRUE, outputs estimate for each
 #'     simulated dataset. If FALSE, just outputs summary statistics across all
-#'     simulated datasets.
+#'     simulated datasets. See Details.
 #' @param misspecified logical (TRUE/FALSE). If TRUE, both frequentist and
 #'     Bayesian g-computation is performing without adjusting for confounding.
 #'     If FALSE, both models correctly adjust for confounding.
@@ -44,7 +44,11 @@
 #'     The function includes an option to run the N_sims simulations in parallel.
 #'     This is enabled for both Macs and PCs by implementing doParallel, however
 #'     **reproducibility is NOT gauranteed when running in parallel** since set.seed()
-#'     is not respected. set.seed() is respected only when not running in parallel.
+#'     is not respected. set.seed() is respected only when not running in parallel. If output_all=FALSE, keilsim()
+#'     will return a list containing a single matrix of summary statistics called sum_stats.
+#'     If output_all=TRUE, the list will contain two other matrices, bayes_sims and freq_sim. These
+#'     contain the Bayesian and frequentist results for each iteration. See the Vignette
+#'     for descriptions of each column of bayes_sims and freq_sim.
 #' @export
 #' @import Rcpp rstan boot doParallel snow
 
